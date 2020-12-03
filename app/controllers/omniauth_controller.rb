61d304f798b9c766df3098d72ddf60a43d52c04c
Nico Rithner <nicorithner@gmail.com>
2020-12-02 19:33:46 -0700

    Generate Ominauth Controller

---
+++ app/controllers/omniauth_controller.rb
@@ -0,0 +1,37 @@
+class OmniauthController < ApplicationController
+
+  def facebook
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem sigining you in through Facebook. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def github
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem sigining you in through Github. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def google_oauth2
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem sigining you in through Google. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
+    redirect_to new_user_registration_url
+  end
+end
